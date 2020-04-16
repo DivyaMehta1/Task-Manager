1fe1534af12c22b04c2d82d376ebdeb91bafb8a1
DivyaMehta1 <43913340+DivyaMehta1@users.noreply.github.com>
2020-04-16 19:41:55 +0530

    Adding server file

---
+++ server.js
@@ -0,0 +1,22 @@
+const express = require('express')
+
+const { db } = require('./db')
+const todoRoute = require('./routes/todos')
+
+const app = express()
+
+app.use(express.urlencoded({ extended: true }))
+app.use(express.json())
+
+app.use('/', express.static(__dirname + '/public'))
+
+app.use('/todos', todoRoute)
+
+db.sync()
+    .then(() => {
+        app.listen(3000)
+        console.log("Server started")
+    })
+    .catch((err) => {
+        console.error(err)
+    })
