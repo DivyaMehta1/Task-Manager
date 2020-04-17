b332be5ec0916de1ce9aab3c3106732335b529db
DivyaMehta1 <43913340+DivyaMehta1@users.noreply.github.com>
2020-04-17 22:09:21 +0530

    collapse feature added

--- routes/todos.js
+++ routes/todos.js
@@ -3,88 +3,68 @@ const { Todos } = require('../db')
 
 const route = Router()
 
-route.get('/', async(req, res) => {
-    const todos = await Todos.findAll()
+route.get('/:orderby', async(req, res) => {
+    // const sort = document.getElementById('sortby').value
+    const sort = req.params.orderby;
+    const todos = await Todos.findAll({
+        order: [
+            [sort, 'DESC']
+        ]
+    })
     res.send(todos)
 
 })
 
-route.get('/:id', async(req, res) => {
+route.get('/', async(req, res) => {
+    // const todos = await Todos.findAll()
+    const todos = await Todos.findAll()
+
+    res.send(todos)
+})
+
+
+
+route.post('/', async(req, res) => {
+    if (typeof req.body.title !== 'string') {
+        return res.status(400).send({ error: 'Task name not provided' })
+    }
+
+    const newTodo = await Todos.create({
+        title: req.body.title,
+        status: req.body.status,
+        description: req.body.description,
+        priority: req.body.priority,
+        duedate: req.body.duedate,
+    })
+
+    res.status(201).send({ success: 'New task added', data: newTodo })
+})
+
+route.patch('/:id', async(req, res) => {
+    console.log("patch hit")
     if (isNaN(Number(req.params.id))) {
         return res.status(400).send({
-            error: 'todo id must be an integer',
+            error: 'Todo ID must be an integer'
         })
     }
 
     const todo = await Todos.findByPk(req.params.id)
+    console.log(todo);
 
     if (!todo) {
         return res.status(404).send({
-            error: 'No todo found with id = ' + req.params.id,
+            error: " No Todo found with id = " + req.params.id
         })
     }
-    res.send(todo)
+
+    todo.duedate = req.body.update_duedate
+    todo.priority = req.body.update_priority
+        //  if(todo.status !== req.body.update_status){
+    todo.status = req.body.update_status
+
+    //db save
+    await todo.save()
+    res.status(201).send({ success: "Updated Successfully", data: todo })
 })
 
-route.post('/', async(req, res) => {
-    try {
-
-        if (typeof req.body.title == null) {
-            return res.status(400).send({ error: 'Task not appropriate' })
-        }
-
-    } catch (err) {
-        console.log(err);
-    }
-    if (req.body.status === "") {
-        req.body.status = 'Incomplete'
-    }
-    if (req.body.priority === "") {
-        req.body.priority = 'Medium'
-    }
-    if (req.body.duedate === "") {
-        req.body.duedate = new Date();
-    }
-    try {
-        const newTodo = await Todos.create({
-            title: req.body.title,
-            description: req.body.description,
-            duedate: req.body.duedate,
-            priority: req.body.priority,
-            status: req.body.status,
-        })
-
-        res.status(201).send({ success: 'New task added', data: newTodo })
-
-    } catch (e) {
-        console.error(e);
-    }
-
-
-})
-route.patch('/todos/:id', async(req, res) => {
-    res.send("YOu are trying to update todos with id " + req.params.id)
-
-})
-route.get('/todos/notes', async(req, res) => {
-    res.send("YOu are trying to access notes  " + req.params.id)
-
-})
-route.post('/todos/notes/:id', async(req, res) => {
-    res.send("YOu are trying to update notes with id " + req.params.id)
-
-})
-route.delete('/:id', function(req, res, next) {
-
-    Todos.findByPk(req.params.id).then((todo) => {
-        if (!todo) {
-            return res.status(404).send({
-                error: 'No todo found with id = ' + req.params.id,
-            })
-        }
-        return todo.destroy();
-    }).then(() => {
-        res.redirect('/');
-    });
-});
 module.exports = route
