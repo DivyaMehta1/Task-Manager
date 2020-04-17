973227b53a98fab5f164c182c13dcba2e1230f6a
DivyaMehta1 <43913340+DivyaMehta1@users.noreply.github.com>
2020-04-17 15:13:41 +0530

    collapse feature added to TaskList

--- public/script.js
+++ public/script.js
@@ -1,19 +1,16 @@
-window.onload = getTaskPrioritySorted()
+//window.onload = getTaskPrioritySorted()
 var list = document.querySelector('ul');
+var submit = document.getElementById('submit');
 
-let submit = document.getElementById('submit')
 submit.onclick = function() {
-    getTasks()
-}
-async function getTaskPrioritySorted() {
-    const todos = await getTasks();
-    const html =
-        todos.sort(sortByPriority(todos))
-        .map(todos => {
-            return `<li><a href="">${todos.title}</a></li>`
-        }).join('')
-    console.log(html);
-    document.querySelector('#myUL').insertAdjacentHTML("afterbegin", html)
+    const title = document.getElementById('title').value
+    const description = document.getElementById('description').value
+    const status = document.getElementById('status').value
+    const duedate = document.getElementById('duedate').value
+    const priority = document.getElementById('priority').value
+    const note = document.getElementById('note').value
+
+    addNewTodoJson(title, description, duedate, status, priority, note)
 }
 
 async function getTaskPrioritySorted() {
@@ -21,30 +18,41 @@ async function getTaskPrioritySorted() {
     const html =
         todos.sort(sortByPriority(todos))
         .map(todos => {
-            return `<li><a href="">${todos.title}</a></li>`
+            return `<li><a href="/todos/${todos.id}">${todos.title}</a></li>`
         }).join('')
     console.log(html);
     document.querySelector('#myUL').insertAdjacentHTML("afterbegin", html)
 }
+
+async function getTodos() {
+    const resp = await fetch('/todos', { method: 'GET' })
+    const todos = await resp.json()
+    console.log(todos)
+
+    for (const element in todos) {
+        // console.log(`${element}: ${todos[element]}`);
+
+        var ul = document.getElementById('myUL')
+
+        var li = document.createElement("li");
+        li.setAttribute('id', todos[element].title);
+        li.setAttribute('class', 'expandable')
+        li.appendChild(document.createTextNode(todos[element].title));
+        ul.appendChild(li);
+    }
+}
+
+
+
 //getTasks from GET/todos
 async function getTasks() {
 
     const resp = await fetch('/todos')
     const todos = await resp.json()
     return todos;
-    /*const html = todos
-        .sort(sortByPriority(todos))
-        .map(todos => {
-            return `<li><a href="">${todos.title}</a></li>`
-        }).join('')
-    console.log(html);
-    document.querySelector('#myUL').insertAdjacentHTML("afterbegin", html)
-*/
-
 }
 
 function sortByPriority(data) {
-    // const data = getTasks();
     var priorities = {
         'High': 0,
         'Medium': 1,
@@ -59,7 +67,7 @@ function LoadTasks() {
     const todos = getTasks();
     const html = todos
         .map(todos => {
-            return `<tr><a href="">${todos.title}</tr>`
+            return `<li><a href="${todos.id}">${todos.title}</li>`
         }).join('')
     console.log(html);
     document.querySelector('#myUL').insertAdjacentHTML("afterbegin", html)
@@ -75,19 +83,26 @@ async function addNewTodoUrlEncoded(task, done, due) {
         body: `task=${task}&done=${done}&due=2020-04-05`
     })
 }
+async function addTodosList() {
+    const resp = await fetch('/todos', { method: 'GET' })
+    const todos = await resp.json()
+    const len = todos.length - 1;
+    console.log(len)
+    var ul = document.getElementById('myUL')
 
-async function addNewTodoJson(todo) {
+    var li = document.createElement("li");
+    li.setAttribute('id', todos[len].title);
+    li.appendChild(document.createTextNode(todos[len].title));
+    ul.appendChild(li);
+}
+
+async function addNewTodoJson(title, description, duedate, status, priority, note) {
     const resp = await fetch('/todos', {
         method: 'POST',
         headers: {
             'Content-Type': 'application/json'
         },
-        body: JSON.stringify({
-            todo: {
-                todo: "John",
-                email: "john@example.com"
-            }
-
-        })
+        body: JSON.stringify(title, description, duedate, status, priority, note)
     })
+    getTaskPrioritySorted()
 }
